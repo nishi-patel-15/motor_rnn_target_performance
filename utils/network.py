e5aa084dcd524b3d1dadb28ab2ab05e677e807cb
ziyuw2 <hermeow@hotmail.com>
2025-07-16 20:07:02 -0400

    first commit

---
+++ utils/network.py
@@ -0,0 +1,227 @@
+import os
+import numpy as np
+import sklearn.linear_model as lm
+import matplotlib.pyplot as plt
+from IPython import display
+
+################################################
+#                                              #
+#         Network definition                   #
+#                                              #
+################################################
+
+# @title RNN encoder
+class RNN(object):
+    """
+    Class implementing a recurrent network (not following Dale's law).
+
+    Parameters:
+    -----------
+    * N: number of neurons
+    * N_in: how many inputs can the network have
+    * N_out: how many neurons are recorded by external device
+    * g: recurrent coupling strength
+    * p: connection probability
+    * tau: neuron time constant
+    * dt: set dt for simulation
+    * delta: defines initial learning rate for FORCE
+    * P_plastic: how many neurons are plastic in the recurrent network
+    """
+    def __init__(self, N=800, g=1.5, p=0.1, tau=0.1, dt=0.01,
+                 N_in=6):
+        # set parameters
+        self.N = N
+        self.g = g
+        self.p = p
+        self.K = int(p*N)
+        self.tau = tau
+        self.dt = dt
+
+        # create recurrent W
+        mask = np.random.rand(self.N,self.N)<self.p
+        np.fill_diagonal(mask,np.zeros(self.N))
+        self.mask = mask
+        self.W = self.g / np.sqrt(self.K) * np.random.randn(self.N,self.N) * mask
+
+        # create Win and Wout
+        self._N_in = N_in
+        self.W_in = (np.random.rand(self.N, self._N_in)-0.5)*2.
+
+    @property
+    def N_in(self):
+        return self._N_in
+
+    @N_in.setter
+    def N_in(self, value):
+        self._N_in = value
+        self.W_in = (np.random.rand(self.N, self._N_in)-0.5)*2.
+
+    def save(self,filename):
+        np.savez(
+            filename,
+            N = self.N,
+            K = self.K,
+            tau = self.tau,
+            g = self.g,
+            p = self.p,
+            dt = self.dt,
+            W_in = self.W_in,
+            W = self.W,
+            N_in = self._N_in,
+        )
+
+    def load(self,filename):
+        net = np.load(filename+'.npz')
+        self.N = int(net['N'])
+        self.dt = float(net['dt'])
+        self.K = int(net['K'])
+        self.tau = float(net['tau'])
+        self.g = float(net['g'])
+        self.p = float(net['p'])
+        self.W_in = net['W_in']
+        self.W = net['W']
+        self._N_in = int(net['N_in'])
+
+    def update_activation(self):
+        self.z = np.tanh(self.r)
+
+    def update_neurons(self,ext):
+        self.r = self.r + self.dt/self.tau * \
+             (-self.r + np.dot(self.W, self.z) + np.dot(self.W_in,ext))
+
+        self.update_activation()
+
+    def simulate(self, T, ext=None, r0=None):
+
+        # define time
+        time = np.arange(0,T,self.dt)
+        tsteps = int(T/self.dt)
+
+        # create input in case no input is given
+        if ext is None:
+            ext = np.zeros((tsteps,self.N_in))
+
+        # check if input has the right shape
+        if ext.shape[0]!=tsteps or ext.shape[1]!=self.N_in:
+            print('ERROR: stimulus shape should be (time x number of input nodes)')
+            return
+
+        # set initial condition
+        if r0 is None:
+            self.r = (np.random.rand(self.N)-0.5)*2.
+        else:
+            self.r = r0
+        self.update_activation()
+
+        # start simulation
+        record_r = np.zeros((tsteps,self.N))
+        record_r[0,:] = self.r
+        for i in range(1,tsteps):
+            self.update_neurons(ext=ext[i])
+            # store activity
+            record_r[i,:] = self.r
+        return time, record_r, np.tanh(record_r)
+
+    def relearn(self, trials, ext, ntstart, decoder, feedback, target, delta=1.,
+                wplastic=None):
+        """
+        Args
+          self.z: RNN network's activation
+          ext (np.array): stimuli (n_targets, n timesteps, n_targets)
+          decoder (np.array): (N units, 2d coordinates) decoder weights
+          feedback (np.array): (N units, 2d coordinates) feedback weights
+          target: (n_targets, N timesteps, 2d coordinates) target coordinates
+
+        Returns:
+
+          loss (np.array): loss by trial
+        """
+        # get number of timesteps within trial
+        tsteps = ext.shape[1]
+
+        # set up learning
+        if wplastic is None:
+            self.W_plastic = [np.where(self.W[i,:]!=0)[0] for i in range(self.N)]
+        else:
+            self.W_plastic = wplastic
+        self.P = [1./delta*np.eye(len(self.W_plastic[i])) for i in range(len(self.W_plastic))]
+
+        # create n trials of target indices chosen from 0 to 5
+        order = np.random.choice(range(ext.shape[0]), trials, replace=True)
+
+        # initialize calculated loss per trial
+        record_loss = np.zeros(trials)
+
+        # loop over trials
+        for t in range(trials):
+
+            # initialize loss
+            loss = 0.
+            self.r = (np.random.rand(self.N)-0.5)*2.
+            self.update_activation()
+
+            # loop over time
+            for i in range(1,tsteps):
+
+                # update units
+                self.update_neurons(ext=ext[order[t],i])
+
+                # learn
+                if i > ntstart and i%2==0:
+
+                    # decode network's predicted
+                    # target coordinates
+                    c = decoder @ self.z
+
+                    # calculate prediction error between
+                    # decoded and true target coordinates (2,)
+                    errc = c - target[order[t], i]
+
+                    # calculate the error update assigned to each weight
+                    err1 = feedback @ errc
+
+                    # calculate loss
+                    loss += np.mean(err1**2)
+
+                    # update plastic recurrent weights
+                    for j in range(self.N):
+                        z_plastic = self.z[self.W_plastic[j]]
+                        pz = np.dot(self.P[j], z_plastic)
+                        norm = (1. + np.dot(z_plastic.T,  pz))
+                        self.P[j] -= np.outer(pz, pz)/norm
+
+                        # use error-transformed feedbacks to update
+                        # plastic weights
+                        self.W[j, self.W_plastic[j]] -= err1[j] * pz / norm
+
+            # tape loss
+            record_loss[t] = loss
+            print('Loss in Trial %d is %.5f'%(t+1,loss))
+        return record_loss
+
+    def calculate_manifold(self, trials, ext, ntstart):
+        tsteps = ext.shape[1]
+        T = self.dt*tsteps
+        points = (tsteps-ntstart)
+        activity = np.zeros((points*trials,self.N))
+        order = np.random.choice(range(ext.shape[0]),trials,replace=True)
+        for t in range(trials):
+            time, r, z = self.simulate(T,ext[order[t]])
+            activity[t*points:(t+1)*points,:] = z[ntstart:,:]
+        cov = np.cov(activity.T)
+        ev,evec = np.linalg.eig(cov)
+        pr = np.round(np.sum(ev.real)**2/np.sum(ev.real**2)).astype(int)
+        xi = activity @ evec.real
+        return activity,cov,ev.real,evec.real,pr,xi,order
+
+def save_RNN(network, savedir:str):
+  """write RNN object and weights in savedir
+  """
+  network.save(savedir+'network')
+  np.save(savedir+'W_initial', network.W)
+
+def save_RNN_sinewave(network, savedir:str):
+  """write RNN sinewave object and weights in savedir
+  """
+  network.save(savedir + 'network_sinewave')
+  np.save(savedir + 'W_initial_sinewave', network.W)
